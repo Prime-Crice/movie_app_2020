1e24fa3cbd42b4f11899ff971f08d40d22ed2100
Jin-Hun Heo <jin-hun@Jin-Hunui-MacBookPro.local>
2020-05-17 12:48:00 +0900

    Updates

---
+++ precache-manifest.447c360299637617e9ca184fe257eabe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7235955407e5c26525a65e3ad304a36",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7f864b4bee8fd008bfa5",
+    "url": "/movie_app_2020/static/css/main.5083c271.chunk.css"
+  },
+  {
+    "revision": "68ebd2a77aba54ef002e",
+    "url": "/movie_app_2020/static/js/2.a1f3f838.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.a1f3f838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f864b4bee8fd008bfa5",
+    "url": "/movie_app_2020/static/js/main.4ff814f0.chunk.js"
+  },
+  {
+    "revision": "9927c9d5c16bc5ac0d63",
+    "url": "/movie_app_2020/static/js/runtime-main.72517e8e.js"
+  }
+]);
